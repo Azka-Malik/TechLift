410a5fc4b7c34c26d0ed494f288146617bfab39b
Admin <Admin@USERTL6.tpsonline.com>
2022-10-27 11:08:14 +0500

    Imageserver

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const port = 3000
+const path = require('path')
+
+app.get('/user', (req, res) => {
+  res.send('Hello World')
+})
+
+app.get('/pic', (req, res) => {
+  let ImagePath = path.join(__dirname,'peach-rose.jpg');
+  res.sendFile(ImagePath)
+})
+
+app.get('/dynamicimage', (req, res) => {
+  let ImageName = req.query.imagename
+  let ImagePath = path.join(__dirname, ImageName);
+  
+  res.sendFile(ImagePath)
+  //res.send(ImageName)
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
+
