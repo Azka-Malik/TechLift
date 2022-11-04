d4deb37af0bfa99a367273739dd4239d84ddac2c
Azka-Malik <azkamalik309@gmail.com>
2022-11-04 20:03:34 +0500

    All tasks

--- index.js
+++ index.js
@@ -3,21 +3,38 @@ const app = express()
 const port = 3000
 const path = require('path')
 
+//Create Server
 app.get('/user', (req, res) => {
   res.send('Hello World')
 })
 
+//Render Image
 app.get('/pic', (req, res) => {
   let ImagePath = path.join(__dirname,'peach-rose.jpg');
   res.sendFile(ImagePath)
 })
 
+
+//Dynamic Image and Not Found Error
 app.get('/dynamicimage', (req, res) => {
-  let ImageName = req.query.imagename
-  let ImagePath = path.join(__dirname, ImageName);
-  
-  res.sendFile(ImagePath)
+  ImageName = req.query.imagename
+  //let ImagePath = path.join(__dirname, ImageName);
+  if(ImageName =='image1.jpg')
+  {
+    let ImagePath = path.join(__dirname, ImageName);
+    res.sendFile(ImagePath)
+  }
+  else if(ImageName == 'image2.jpg')
+  {
+    ImagePath = path.join(__dirname, ImageName);
+    res.sendFile(ImagePath)
+  }
+  else{
+    res.send('Image Not Found')
+  }
+ // res.sendFile(ImagePath)
 })
+
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
